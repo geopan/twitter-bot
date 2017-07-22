c95a17311e3891de0c65d3abb02996adede634a0
Guillaume <gdeboyer@gmail.com>
2017-07-22 10:58:54 +1000

    some improvement..

--- app.js
+++ app.js
@@ -1,88 +1,125 @@
-const {log, error} = require('console');
+'use strict';
 
 require('dotenv').config();
+const {
+  log,
+  error
+} = require('console');
 
 const queries = require('./search');
+const Twitter = require('twitter');
+const {
+  twitter,
+  instagram,
+  mongo
+} = require('./config.js');
 
-var Twitter = require('twitter');
-var config = require('./config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
 
-var interval = process.env.INTERVAL || 10000;
+let TweetSchema = new mongoose.Schema({
+  tid: String,
+  tweet: Object
+});
 
-var T = new Twitter(config);
+TweetSchema.index({
+  'tid': 1
+});
 
-var Twitter = require('twitter');
-var config = require('./config.js');
-var T = new Twitter(config);
+const Tweet = mongoose.model('Tweet', TweetSchema);
+
+var T = new Twitter(twitter);
 
 
 log('bot started...\n');
 
-function like() {
+async function like() {
 
-  for ( let search of queries) {
+  try {
 
-    let params = {
-      q: search.q,
-      count: 20,
-      result_type: 'recent',
-      lang: 'en'
-    };
+    for (let search of queries) {
 
-    T.get('search/tweets', params, function(err, data, response) {
-      if(!err){
-        
-        for ( let tweet of data.statuses ) {
-          // Get the tweet Id from the returned data
-          let id = { id: tweet.id_str }
-          let {text, retweet_count, favorite_count} = tweet;
+      let params = {
+        q: search.q,
+        count: 5,
+        result_type: 'recent',
+        lang: 'en'
+      };
 
-          if (tweet.retweet_count > search.limit) {
+      log(`get: search/tweets ${params.q}`);
+      let tweetResponse = await T.get('search/tweets', params); //, async function(err, data, response) {
 
-            T.post('statuses/retweet', id, function(err, response){
-              // If the retweet fails, log the error message
-              if(err){
-               // error(err[0].message);
-               return;
-              }
-              // If the retweet is successful, log the url of the tweet
-              else{
-                let username = response.user.screen_name;
-                let tweetId = response.id_str;
-                log('retweeted: ', `https://twitter.com/${username}/status/${tweetId}`)
-              }
-            });
+      for (let tweet of tweetResponse.statuses) {
 
-          } else {
-            
-            T.post('favorites/create', id, function(err, response){
-        // If the favorite fails, log the error message
-              if(err){
-                return;
-              }
-              // If the favorite is successful, log the url of the tweet
-              else{
-                let username = response.user.screen_name;
-                let tweetId = response.id_str;
-                log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
-              }
-            });
-          }
-
+        // Get the tweet Id from the returned data
+        let id = {
+          id: tweet.id_str
         }
 
-      } else {
-        error(err);
-      }
-    })
+        let check = await Tweet.findOne({
+          "tid": tweet.id_str
+        });
 
+        if (check) {
+          log(`${id} already processed`);
+          continue;
+        }
+
+        let tt = new Tweet({
+          tweet: tweet,
+          tid: tweet.id_str
+        });
+
+        let {
+          text,
+          retweet_count,
+          favorite_count
+        } = tweet;
+
+        if (tweet.retweet_count > search.limit) {
+          log(`post: statuses/retweet ${id}`);
+          let post = await T.post('statuses/retweet', id);
+
+          await tt.save();
+          let username = post.user.screen_name;
+          let tweetId = post.id_str;
+          log('retweeted: ', `https://twitter.com/${username}/status/${tweetId}`)
+
+        } else {
+
+          log(`post: favorites/create ${id}`);
+          let post = await T.post('favorites/create', id);
+
+          await tt.save();
+          let username = post.user.screen_name;
+          let tweetId = post.id_str;
+          log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+
+        }
+      }
+    }
+
+
+  } catch (err) {
+    error(err);
   }
 
 };
 
 function run() {
-  setInterval(like, interval);
+  setInterval(like, process.env.INTERVAL || 10000);
 };
 
-run();
 
+mongoose.connect(mongo.uri)
+  .then(() => {
+
+    log('Connection to %s open.', mongo.uri);
+
+    run();
+
+  })
+  .catch((err) => {
+    error(err.message);
+    process.exit(1);
+  });
--- config.js
+++ config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  consumer_key: process.env.CONS_KEY || '',
-  consumer_secret: process.env.CONS_SECRET ||'',
-  access_token_key: process.env.ACCESS_KEY || '',
-  access_token_secret: process.env.ACCESS_SECRET || ''
+  twitter: {
+    consumer_key: process.env.CONS_KEY || '',
+    consumer_secret: process.env.CONS_SECRET ||'',
+    access_token_key: process.env.ACCESS_KEY || '',
+    access_token_secret: process.env.ACCESS_SECRET || ''
+  }, 
+  instagram: {
+    access_token: process.env.IG_ACCES_TOK || '',
+    client_id: process.env.IG_CLIENT_ID || '',
+    client_secret: process.env.IG_SECRET || ''
+  },
+  mongo: {
+    uri: process.env.MONGO_URI || 'mongodb://localhost/geobot',
+  },
 }
