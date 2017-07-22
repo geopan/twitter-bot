4ff0be06451920d5515dc5fa4d224e37c98f829b
Guillaume <gdeboyer@gmail.com>
2017-07-22 11:10:39 +1000

    increase limit

--- search.js
+++ search.js
@@ -3,33 +3,33 @@
 module.exports = [
     {
         q: '#gis -esri -filter:retweets filter:links',
-        limit: 5
+        limit: 20
     }, {
         q: '#geospatial -esri -filter:retweets filter:links',
-        limit: 5
+        limit: 20
     }, {
         q: 'ethereum filter:links',
-        limit: 50
+        limit: 100
     }, {
         q: '#blockchain filter:links',
-        limit: 50
+        limit: 100
     }, {
         q: '#deeplearning filter:links',
-        limit: 50
+        limit: 100
     }, {
         q: '#nodejs filter:links',
-        limit: 50
+        limit: 100
     }, {
         q: '#golang filter:links',
-        limit: 50
+        limit: 100
     }, {
         q: '#TensorFlow  filter:links',
-        limit: 50
+        limit: 100
     }, {
         q: '#kaggle  filter:links',
         limit: 50
     }, {
         q: 'MachineLearning filter:links',
-        limit: 50
+        limit: 100
     }
 ];
